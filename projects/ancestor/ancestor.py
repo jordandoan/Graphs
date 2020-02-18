8eb53917804174a24e9bf98ae19f84f060d882b9
Jordan <jordandoan@hotmail.com>
2020-02-17 20:05:18 -0800

    find ancestor is done

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,23 @@
 
+import collections
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    q = collections.deque()
+    q.append((starting_node, 1))
+    min_id = (99999, 1)
+    level = 0
+    while q:
+        level += 1
+        for _ in range(len(q)):
+            node, cur_lev = q.popleft()
+            parents = [i[0] for i in ancestors if i[1] == node]
+            if parents:
+                for parent in parents:
+                    q.append((parent, level))
+            else:
+                if cur_lev > min_id[1]:
+                    min_id = (node, cur_lev)
+                elif cur_lev == min_id[1]:
+                    min_id = min(min_id, (node, cur_lev), key=lambda t: t[0])
+                if not q:
+                    break
+    return min_id[0] if level > 1 else -1
